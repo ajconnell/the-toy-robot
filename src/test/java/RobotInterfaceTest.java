8b1deb3fbf6b26fc2c80596c842818c29e3033e3
Sempronius <Sempronius>
2021-03-29 12:21:51 +1100

    - Tests for default behaviours on Robot interface using Mockito spy

---
+++ src/test/java/RobotInterfaceTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.spy;
+
+public class RobotInterfaceTest {
+
+    @Test
+    public void assertDefaultPlaceThrowsIllegalArgumentException() {
+        Robot robotInterface = spy(Robot.class);
+        Assertions.assertThrows(IllegalArgumentException.class, robotInterface::place);
+    }
+
+    @Test
+    public void assertDefaultMoveThrowsIllegalArgumentException() {
+        Robot robotInterface = spy(Robot.class);
+        Assertions.assertThrows(IllegalArgumentException.class, robotInterface::move);
+    }
+
+    @Test
+    public void assertDefaultLeftThrowsIllegalArgumentException() {
+        Robot robotInterface = spy(Robot.class);
+        Assertions.assertThrows(IllegalArgumentException.class, robotInterface::left);
+    }
+
+    @Test
+    public void assertDefaultRightThrowsIllegalArgumentException() {
+        Robot robotInterface = spy(Robot.class);
+        Assertions.assertThrows(IllegalArgumentException.class, robotInterface::right);
+    }
+
+}
