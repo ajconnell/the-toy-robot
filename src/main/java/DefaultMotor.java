f51df8048525eb3dc63d522702aefce2d519d9e1
Sempronius <Sempronius>
2021-03-29 18:23:57 +1100

    - Refactor DefaultMotor move to account for bounds of the default perimeter of the board of 5 x 5

--- src/main/java/DefaultMotor.java
+++ src/main/java/DefaultMotor.java
@@ -6,22 +6,31 @@ public class DefaultMotor implements Motor {
     public void move(Geolocator geolocator) {
         Direction direction = geolocator.getDirection();
         Point position = geolocator.getPosition();
+        Point perimeter = geolocator.getPerimeter();
 
         int currentX = position.x;
         int currentY = position.y;
 
         switch (direction) {
             case NORTH:
-                position.move(currentX, currentY + 1);
+                if (currentY < perimeter.y) {
+                    position.move(currentX, currentY + 1);
+                }
                 break;
             case EAST:
-                position.move(currentX + 1, currentY);
+                if (currentX < perimeter.x) {
+                    position.move(currentX + 1, currentY);
+                }
                 break;
             case SOUTH:
-                position.move(currentX, currentY - 1);
+                if (currentY > 0) {
+                    position.move(currentX, currentY - 1);
+                }
                 break;
             case WEST:
-                position.move(currentX - 1, currentY);
+                if (currentX > 0) {
+                    position.move(currentX - 1, currentY);
+                }
                 break;
         }
     }
--- src/main/java/Geolocator.java
+++ src/main/java/Geolocator.java
@@ -2,12 +2,20 @@ import java.awt.*;
 
 public class Geolocator {
 
+    private static final int DEFAULT_PERIMETER = 4;
+
+    private final Point perimeter;
     private Point position;
     private Direction direction;
 
     public Geolocator() {
+        this.perimeter = new Point(DEFAULT_PERIMETER, DEFAULT_PERIMETER);
         this.position = null;
         this.direction = null;
+    }
+
+    public Point getPerimeter() {
+        return this.perimeter;
     }
 
     public void setPosition(Point position) {
