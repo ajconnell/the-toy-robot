000e9cdb240107addfbd9eb1fb9902eaf5faa337
Sempronius <Sempronius>
2021-03-29 18:28:54 +1100

    - Add tests to confirm movement and boundary limitations

---
+++ src/test/java/DefaultMotorTest.java
@@ -0,0 +1,106 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DefaultMotorTest {
+
+    @Test
+    public void assertShouldFacilitateMoveNorthWhenNotAtEdgeOfBoard() {
+        Geolocator geolocator = new Geolocator();
+        geolocator.setDirection(Direction.NORTH);
+        geolocator.setPosition(new Point(4, 3));
+
+        DefaultMotor motor = new DefaultMotor();
+        motor.move(geolocator);
+
+        Assertions.assertEquals(4, geolocator.getPosition().y);
+    }
+
+    @Test
+    public void assertShouldFacilitateMoveSouthWhenNotAtEdgeOfBoard() {
+        Geolocator geolocator = new Geolocator();
+        geolocator.setDirection(Direction.SOUTH);
+        geolocator.setPosition(new Point(4, 1));
+
+        DefaultMotor motor = new DefaultMotor();
+        motor.move(geolocator);
+
+        Assertions.assertEquals(0, geolocator.getPosition().y);
+    }
+
+    @Test
+    public void assertShouldFacilitateMoveEastWhenNotAtEdgeOfBoard() {
+        Geolocator geolocator = new Geolocator();
+        geolocator.setDirection(Direction.EAST);
+        geolocator.setPosition(new Point(3, 3));
+
+        DefaultMotor motor = new DefaultMotor();
+        motor.move(geolocator);
+
+        Assertions.assertEquals(4, geolocator.getPosition().x);
+    }
+
+    @Test
+    public void assertShouldFacilitateMoveWestWhenNotAtEdgeOfBoard() {
+        Geolocator geolocator = new Geolocator();
+        geolocator.setDirection(Direction.WEST);
+        geolocator.setPosition(new Point(1, 3));
+
+        DefaultMotor motor = new DefaultMotor();
+        motor.move(geolocator);
+
+        Assertions.assertEquals(0, geolocator.getPosition().x);
+    }
+
+    @Test
+    public void assertShouldNotMoveNorthWhenAtEdgeOfBoard() {
+        Geolocator geolocator = new Geolocator();
+        geolocator.setDirection(Direction.NORTH);
+        geolocator.setPosition(new Point(4, 4));
+
+        DefaultMotor motor = new DefaultMotor();
+        motor.move(geolocator);
+
+        Assertions.assertEquals(4, geolocator.getPosition().y);
+    }
+
+    @Test
+    public void assertShouldNotMoveSouthWhenAtEdgeOfBoard() {
+        Geolocator geolocator = new Geolocator();
+        geolocator.setDirection(Direction.SOUTH);
+        geolocator.setPosition(new Point(4, 0));
+
+        DefaultMotor motor = new DefaultMotor();
+        motor.move(geolocator);
+
+        Assertions.assertEquals(0, geolocator.getPosition().y);
+    }
+
+    @Test
+    public void assertShouldNotMoveEastWhenAtEdgeOfBoard() {
+        Geolocator geolocator = new Geolocator();
+        geolocator.setDirection(Direction.EAST);
+        geolocator.setPosition(new Point(4, 3));
+
+        DefaultMotor motor = new DefaultMotor();
+        motor.move(geolocator);
+
+        Assertions.assertEquals(4, geolocator.getPosition().x);
+    }
+
+    @Test
+    public void assertShouldNotMoveWestWhenAtEdgeOfBoard() {
+        Geolocator geolocator = new Geolocator();
+        geolocator.setDirection(Direction.WEST);
+        geolocator.setPosition(new Point(0, 3));
+
+        DefaultMotor motor = new DefaultMotor();
+        motor.move(geolocator);
+
+        Assertions.assertEquals(0, geolocator.getPosition().x);
+    }
+
+}
